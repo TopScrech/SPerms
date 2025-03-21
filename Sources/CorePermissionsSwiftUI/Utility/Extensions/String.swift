e90cd013f77e30623936c3e9fe19612ffaf15385
TopScrech <sergei_saliukov@icloud.com>
2025-03-21 02:59:11 +0100

    Revert "Removed an unnecessary file for legacy loc"
    This reverts commit a97af6e1532d98910d0e9aca5294c92f3677d8a9.

---
+++ Sources/CorePermissionsSwiftUI/Utility/Extensions/String.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+extension String {
+    // https://stackoverflow.com/questions/25081757/whats-nslocalizedstring-equivalent-in-swift
+    /// Swifty wrapper for accessing NSLocalizedString with only key parameter
+    var localized: String {
+        return NSLocalizedString(self, tableName: nil, bundle: .module, value: "", comment: "")
+    }
+}
